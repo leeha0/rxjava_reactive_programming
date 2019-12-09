d3c9fca343a7fc67172358364d8da50aa344606d
leeha0 <gkdud2837@gmail.com>
2019-12-09 22:58:04 +0900

    Chapter1 - Opservable operator

--- src/main/java/chapter1/L07_MethodChainSample.java
+++ src/main/java/chapter1/L07_MethodChainSample.java
@@ -1,6 +1,7 @@
 package chapter1;
 
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
 
 public class L07_MethodChainSample {
     public static void main(String[] args) {
@@ -10,5 +11,12 @@ public class L07_MethodChainSample {
                     .map(data -> data * 100);
 
         flowable.subscribe(data -> System.out.println("data=" + data));
+
+        Observable<Integer> observable =
+                Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                    .filter(data -> data % 2 == 0)
+                    .map(data -> data * 100);
+
+        observable.subscribe(data -> System.out.println("data=" + data));
     }
 }
