8d959e7af8f6ec8e939dfa428a0224b7d067bb3e
leeha0 <gkdud2837@gmail.com>
2019-12-11 01:51:38 +0900

    Chapter1 - L10 예제 분석

--- src/main/java/chapter1/L10_EffectedSample.java
+++ src/main/java/chapter1/L10_EffectedSample.java
@@ -6,18 +6,22 @@ import java.util.concurrent.TimeUnit;
 
 public class L10_EffectedSample {
 
+    // 계산 방법을 나타내는 enum 객체
     private enum State {
         ADD, MULTIPLE
     }
 
+    // 계산 방법
     private static State calcMethod;
 
     public static void main(String[] args) throws Exception {
+        // 계산 방법을 덧셈으로 설정한다.
         calcMethod = State.ADD;
 
+        // 300 밀리초마다 데이터를 통지하는 Flowable을 생성한다.
         Flowable<Long> flowable = Flowable.interval(300L, TimeUnit.MILLISECONDS)
-                .take(7)
-                .scan((sum, data) -> {
+                .take(7) // 7건까지 통지한다.
+                .scan((sum, data) -> { // 각 데이터를 계산한다.
                     if (calcMethod == State.ADD) {
                         return sum + data;
                     } else {
@@ -25,12 +29,15 @@ public class L10_EffectedSample {
                     }
                 });
 
+        // 구독하고 받은 데이터를 출력한다.
         flowable.subscribe(data -> System.out.println("data=" + data));
 
+        // 잠시 기다렸다가 계산 방법을 곱셈으로 변환한다.
         Thread.sleep(1000);
         System.out.println("계산 방법 변경");
         calcMethod = State.MULTIPLE;
 
+        // 잠시 기다린다.
         Thread.sleep(2000);
     }
 }
