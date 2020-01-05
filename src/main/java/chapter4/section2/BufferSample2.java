def40677f3d8c3d39991c48b6cf3d120c0692d1c
leeha0 <gkdud2837@gmail.com>
2020-01-05 23:49:38 +0900

    Chapter4 - BufferSample2.java 주석처리

--- src/main/java/chapter4/section2/BufferSample2.java
+++ src/main/java/chapter4/section2/BufferSample2.java
@@ -9,16 +9,16 @@ import java.util.concurrent.TimeUnit;
 
 public class BufferSample2 {
     public static void main(String[] args) throws InterruptedException {
-        Flowable<List<Long>> flowable =
-                // 300밀리초마다 숫자를 통지하는 Flowable 생성
-                Flowable.interval(300L, TimeUnit.MILLISECONDS)
-                        // 7건까지 통지한다.
-                        .take(7)
-                        // 버퍼 구간을 나누는 Flowable 생성한다.
-                        .buffer(() -> Flowable.timer(1000L, TimeUnit.MILLISECONDS));
-
-        flowable.subscribe(new DebugSubscriber<>());
-
-        Thread.sleep(4000L);
+//        Flowable<List<Long>> flowable =
+//                // 300밀리초마다 숫자를 통지하는 Flowable 생성
+//                Flowable.interval(300L, TimeUnit.MILLISECONDS)
+//                        // 7건까지 통지한다.
+//                        .take(7)
+//                        // 버퍼 구간을 나누는 Flowable 생성한다.
+//                        .buffer(() -> Flowable.timer(1000L, TimeUnit.MILLISECONDS));
+//
+//        flowable.subscribe(new DebugSubscriber<>());
+//
+//        Thread.sleep(4000L);
     }
 }
