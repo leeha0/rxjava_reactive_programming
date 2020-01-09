6c32849df1f8434e8e01601120368fa3e07e30cf
이하영 <leeha@wemakeprice.com>
2020-01-09 10:25:01 +0900

    Chapter4 - doOnNext를 통해 ms 출력 변경

--- src/main/java/chapter4/section4/ConcatEagerSample.java
+++ src/main/java/chapter4/section4/ConcatEagerSample.java
@@ -10,33 +10,35 @@ import java.util.concurrent.TimeUnit;
 public class ConcatEagerSample {
     public static void main(String[] args) throws InterruptedException {
         // 결합 대상
-        Flowable<String> flowable1 =
+        Flowable<Long> flowable1 =
                 // 300 밀리초마다 데이터를 통지한다.
                 Flowable.interval(300L, TimeUnit.MILLISECONDS)
                         // 5건까지 통지한다.
                         .take(5)
-                        .map(data -> {
+                        .doOnNext((aLong) -> {
                             // 통지 시 시스템 시각도 데이터에 추가한다.
                             long time = System.currentTimeMillis();
-                            return time + "ms: [" + data + "] ";
+                            System.out.println(time + "ms: [" + (aLong) + "] ");
                         });
 
+
         // 결합 대상
-        Flowable<String> flowable2 =
+        Flowable<Long> flowable2 =
                 // 500 밀리초마다 데이터를 통지한다.
                 Flowable.interval(500L, TimeUnit.MILLISECONDS)
                         // 5건까지 통지한다.
                         .take(5)
                         // 100을 더한다.
-                        .map(data -> {
+                        .map(data -> data + 100L)
+                        .doOnNext((aLong) -> {
                             // 통지 시 시스템 시각도 데이터에 추가한다.
                             long time = System.currentTimeMillis();
-                            return time + "ms: [" + (data + 100L) + "] ";
+                            System.out.println(time + "ms: [" + (aLong) + "] ");
                         });
 
         // 여러 개의 Flowable을 결합한다.
-        List<Flowable<String>> sources = Arrays.asList(flowable1, flowable2);
-        Flowable<String> result = Flowable.concatEager(sources);
+        List<Flowable<Long>> sources = Arrays.asList(flowable1, flowable2);
+        Flowable<Long> result = Flowable.concatEager(sources);
 
         // 구독한다.
         result.subscribe(new DebugSubscriber<>());
